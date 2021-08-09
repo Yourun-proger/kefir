04f22b2860c216c39cbfd0cd068bd2236380ab0e
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-08-09 17:01:59 +0300

    Add support for nested data

--- kefir.py
+++ kefir.py
@@ -1,9 +1,13 @@
+import re
 def dump_to_dict(model, one=True):
     if one:
         dct = {}
         for key, value in model.__dict__.items():
             if not key.startswith('_'):
-                dct[key] = value
+                if re.search('<__main__..*>', str(value)):
+                    dct[key] = dump_to_dict(value)
+                else:
+                    dct[key] = value
         try:
             dct['id'] = model.id
         except AttributeError:
@@ -15,7 +19,10 @@ def dump_to_dict(model, one=True):
             model_dct = {}
             for key, value in m.__dict__.items():
                 if not key.startswith('_'):
-                    model_dct[key] = value
+                    if re.search('<__main__..*>', str(value)):
+                        model_dct[key] = dump_to_dict(value)
+                    else:
+                        model_dct[key] = value
             try:
                 model_dct['id'] = model.id
             except AttributeError:
