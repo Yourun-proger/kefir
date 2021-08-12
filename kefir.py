3205fd8820eaf67a210a9ec9841cf273f28bd15b
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-08-12 14:03:44 +0300

    fix potential bug and remove multi choose
    drop bad code for db models and multi type.
    potential bug:
    this regexp catch class only on file where we use kefir, but it isn't work for classes from other modules

--- kefir.py
+++ kefir.py
@@ -1,31 +1,13 @@
-import re
-def dump_to_dict(model, one=True):
-    if one:
-        dct = {}
-        for key, value in model.__dict__.items():
-            if not key.startswith('_'):
-                if re.search('<__main__..*>', str(value)):
-                    dct[key] = dump_to_dict(value)
-                else:
-                    dct[key] = value
-        try:
-            dct['id'] = model.id
-        except AttributeError:
-            pass
-        return dct
-    else:
-        models = []
-        for m in model:
-            model_dct = {}
-            for key, value in m.__dict__.items():
-                if not key.startswith('_'):
-                    if re.search('<__main__..*>', str(value)):
-                        model_dct[key] = dump_to_dict(value)
-                    else:
-                        model_dct[key] = value
-            try:
-                model_dct['id'] = model.id
-            except AttributeError:
-                pass
-            models.append(model_dct)
-        return models
+def dump(model):
+    dct = {}
+    for key, value in model.__dict__.items():
+        if not key.startswith('_'):
+            if not isinstance(value, int)  and \
+                not isinstance(value, str) and \
+                not isinstance(value, bool) and \
+                not isinstance(value, dict) and \
+                not isinstance(value, float):
+                dct[key] = dump(value)
+            else:
+                dct[key] = value
+    return dct
