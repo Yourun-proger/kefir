8d8bed203c906ad4f1a072ded8180a6a0ddaec6b
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-09-17 22:45:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kefir',
-    version='0.1.2',
+    version='0.1.1',
     url='http://github.com/yourun-proger/kefir/',
     license='MIT',
     author='Yourun-Proger',
