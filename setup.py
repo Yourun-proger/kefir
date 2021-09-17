39f3114f4d16663b54725462ebf5ccc023f5fa83
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-09-17 22:44:52 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='kefir',
+    version='0.1.2',
+    url='http://github.com/yourun-proger/kefir/',
+    license='MIT',
+    author='Yourun-Proger',
+    description='convert DB models or custom-class objects to python dict',
+    py_modules=['kefir'],
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
