9a9df97f4ffd54e9010913f6122969298a9d1802
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-09-20 20:46:13 +0300

    Support for DB relations and `__slots__` classes.
    0.1.3 release!!!
    New param `rels` needs for normal db relations support. Also *Kefir* checks classes where used `__slots__`.
    Cool fact: *Kefir* can dump `dataclass`. It is better `as_dict`, because it support nested data. 
    **NOTE:**
    i don't know work this changes for many2many relations or not ¯\_(ツ)_/¯
    Also this code not work with models, where models have > 1 relations, becasue Kefir parse only column values, not relations. True don't khow how i can improve it.

--- kefir.py
+++ kefir.py
@@ -1,17 +1,11 @@
 class Kefir:
-    def __init__(self, session=None, shorcuts={}, ignore=[], objects={}):
+    
+    def __init__(self, session=None, objects={}, rels=[], shorcuts={}, ignore=[]):
         self.session = session  # SQLAlhcemy session
-        self.shorcuts = shorcuts # for change name of tables to your choose
-        # Example
-        # print(kef.dump(order))
-        # {'id':1, 'items': [...], 'users': {'id':84, 'name':'Kefir'}}
-        # this looks bad, because only one user can be owner of order (in your API!)
-        # kef = Kefir(db.session, {'users':'user'})
-        # print(kef.dump(order))
-        # {'id':1, 'items': [...], 'user': {'id':84, 'name':'Kefir'}}
-        # Good!
-        self.ignore = ignore # tables for ignoring
         self.objects = objects # tablename -> Class; {'users':User} for example. this is needed for relations
+        self.rels = rels # some relations of tables. Example: rels={'users':['orders']}. users table is main in rels with orders
+        self.shorcuts = shorcuts  # shorcuts for change table name in converted object
+        self.ignore = ignore  # Table for ignoring
     
     def _is_custom_class(self, obj):
          return not (
@@ -42,30 +36,67 @@ class Kefir:
             return lst
         else:
             obj_type = 'class'
-            if "<class 'sqlalchemy.ext.declarative.api.Model'>" in list(map(str, type(obj).mro())):
+            if "<class 'sqlalchemy.ext.declarative.api.Model'>" in list(map(str, type(obj).mro())): #looks bad :[
                 obj_type = 'model'
             
             if obj_type == 'class':
                 dct = {}
-                for key, value in obj.__dict__.items():
-                    if not key.startswith('_'):
-                        if self._is_custom_class(value):
-                            dct[key] = self.dump(value)
+                try:
+                    obj.__slots__
+                    has_slots = True
+                except AttributeError:
+                    has_slots = False
+                if has_slots:
+                    for key in obj.__slots__:
+                        if self._is_custom_class(getattr(obj, key)):
+                            dct[key] = self.dump(getattr(obj, key))
                         else:
-                            dct[key] = value
+                            dct[key] = getattr(obj, key)
+                else:
+                    for key, value in obj.__dict__.items():
+                        if not key.startswith('_'):
+                            if self._is_custom_class(value):
+                                dct[key] = self.dump(value)
+                            else:
+                                dct[key] = value
                 return dct
             elif obj_type == 'model':
                 dct = {}
+                done = []
                 table = obj.metadata.tables[obj.__tablename__]
                 cols = [str(col[len(obj.__tablename__)+1:]) for col in list(map(str, table.columns))]
                 for col in cols:
                     if self._is_foreign_key(table, col):
                         for i in  self._is_foreign_key(table, col):
                            if i[0] not in self.ignore:
-                               sql = 'SELECT * FROM '+ i[0]  + ' WHERE ' + i[1] + '= ' + '"' + str(getattr(obj, col)) + '"'
-                               data = self.session.execute(sql).cursor.fetchone()
-                               new_obj = self.objects[i[0]].query.get(data[0])
-                               dct[self.shorcuts.get(i[0], i[0])] = self.dump(new_obj)
-                    else:
-                        dct[col] = getattr(obj, col)
+                               sql = 'SELECT * FROM '+ i[0]  + ' WHERE ' + i[1] + ' = ' + '"' + str(getattr(obj, col)) + '"' + ';'
+                               data = self.session.execute(sql).cursor.fetchall()
+                               cols = self.session.execute(sql).__dict__['_metadata'].keys
+                               if len(data) == 1:
+                                   dct[self.shorcuts.get(i[0], i[0])] = dict(zip(cols, data[0]))                                
+                               else:
+                                   dct[self.shorcuts.get(i[0], i[0])] = [dict(zip(cols, i)) for i in data]
+
+                    dct[col] = getattr(obj, col)
+                if self.rels.get(obj.__tablename__):
+                    for i in self.rels[obj.__tablename__]:
+                        obj_arg = list(obj.__dict__['_sa_instance_state'].__dict__ \
+                                                                    ['manager'].__dict__['local_attrs'][i].__dict__\
+                                                                    [ 'comparator'].__dict__['_parententity'].__dict__ \
+                                                                    ['_init_properties'][i].__dict__ \
+                                                                    ['synchronize_pairs'][0][1].__dict__ \
+                                                                    ['foreign_keys'])[0].__dict__[ '_column_tokens'][2]
+                        
+                        rel_obj_arg = list(obj.__dict__['_sa_instance_state'].__dict__['manager'].__dict__ \
+                                      ['local_attrs'][i].__dict__[ 'comparator'].__dict__ \
+                                      ['_parententity'].__dict__['_init_properties'][i].__dict__ \
+                                      ['synchronize_pairs'][0][1].__dict__['foreign_keys'])[0].__dict__ \
+                                      ['parent'].__dict__[ '_key_label'][len(i)+1:]
+                        sql = 'SELECT * FROM ' + i + ' WHERE ' + rel_obj_arg + ' = ' + '"' + str(getattr(obj, obj_arg)) + '"' + ';'
+                        data = self.session.execute(sql).fetchall()
+                        cols = self.session.execute(sql).__dict__['_metadata'].keys
+                        if len(data) == 1:
+                            dct[self.shorcuts.get(i, i)] = dict(zip(cols, data))
+                        else:
+                            dct[self.shorcuts.get(i, i)] = [dict(zip(cols, i)) for i in data]
                 return dct
