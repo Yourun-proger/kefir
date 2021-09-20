10d0ccb388716de895770d406df0f85752e2300c
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-09-20 20:47:55 +0300

    update version and add `zip_safe=False` param

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name='kefir',
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/yourun-proger/kefir/',
     license='MIT',
     author='Yourun-Proger',
     description='convert DB models or custom-class objects to python dict',
     py_modules=['kefir'],
     platforms='any',
+    zip_safe=False
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
