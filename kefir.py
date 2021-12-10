8c50247dd63615eb2423f38aca3a65b13a18e968
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-12-11 00:16:29 +0300

    Reorganised code
    Make it easy for reading and remove hardcoded values. Today work on `load` method.

--- kefir.py
+++ kefir.py
@@ -12,133 +12,63 @@ except ImportError:
 
 
 class Kefir:
+    def __init__(self):
+        ...  # place for future work
 
-    def __init__(self, session=None, objects={}, rels={}, ignore={}, shorcuts={}):
-        self.session = session  # SQLAlhcemy session
-        self.objects = objects  # tablename -> Class; {'users':User} for example. this is needed for relations
-        self.rels = rels  # some relations of tables. Example: rels={'users':['orders']}. users table is main in rels with orders
-        self.ignore = ignore
-        self.shorcuts = shorcuts
-
-    def _is_custom_class(self, obj):
-        return not (
-                isinstance(obj, int) or \
-                isinstance(obj, str) or \
-                isinstance(obj, bool) or \
-                isinstance(obj, dict) or \
-                isinstance(obj, float)
-        )
-
-    def _is_foreign_key(self, table, col):
-        dct = {}
-        for i in table.columns:
-            rels = []
-            if i.foreign_keys:
-                for j in i.foreign_keys:
-                    rels.append((j._column_tokens[1], j._column_tokens[2]))
-
-            dct[i.name] = rels
-
-        return dct[col]
-
-    def dump(self, obj, ignore=()):
+    def dump(self, obj, ignore=None):
         if isinstance(obj, list):
             lst = []
-            for i in obj:
-                lst.append(self.dump(i))
+            for item in obj:
+                lst.append(self.dump(item))
             return lst
+        dct = {}
+        if obj.__dict__.get('_sa_instance_state'):
+            for k, v in obj.__dict__['_sa_instance_state'].__dict__['manager'].__dict__['local_attrs'].items():
+                item = getattr(obj,k)
+                if item is not ignore:
+                    if isinstance(item, (int, str, bool, dict, float)):
+                        dct[k] = item
+                    else:
+                        if isinstance(item, list):
+                            dct[k] = [self.dump(i, obj) for i in item]
+                        else:
+                            dct[k] = self.dump(item, obj)
         else:
-            obj_type = 'class'
-            if "<class 'sqlalchemy.ext.declarative.api.Model'>" in list(map(str, type(obj).mro())):  # looks bad =_=
-                obj_type = 'model'
+            if hasattr(obj, '__slots__'):
+                for k in obj.__slots__:
+                    if isinstance(getattr(obj,k), (int, str, bool, dict, float)):
+                        dct[k] = getattr(obj, k)
+                    else:
+                        dct[k] = self.dump(getattr(obj, k))
+            for k, v in obj.__dict__.items():
+                if not k.startswith('_'):
+                    if isinstance(v, (int, str, bool, dict, float)):
+                        dct[k] = v
+                    else:
+                        dct[k] = self.dump(v)
 
-            if obj_type == 'class':
-                dct = {}
-                try:
-                    obj.__slots__
-                    has_slots = True
-                except AttributeError:
-                    has_slots = False
-                if has_slots:
-                    for key in obj.__slots__:
-                        if self._is_custom_class(getattr(obj, key)):
-                            dct[key] = self.dump(getattr(obj, key))
-                        else:
-                            dct[key] = getattr(obj, key)
-                else:
-                    for key, value in obj.__dict__.items():
-                        if not key.startswith('_'):
-                            if self._is_custom_class(value):
-                                dct[key] = self.dump(value)
-                            else:
-                                dct[key] = value
-                return dct
-            elif obj_type == 'model':
-                dct = {}
-                done = []
-                table = obj.metadata.tables[obj.__tablename__]
-                cols = [str(col[len(obj.__tablename__) + 1:]) for col in list(map(str, table.columns))]
-                for col in cols:
-                    if self._is_foreign_key(table, col):
-                        for i in self._is_foreign_key(table, col):
-                            if i[0] not in ignore and  i[0] not in self.ignore.get(obj.__tablename__,[]):
-                                sql = 'SELECT * FROM ' + i[0] + ' WHERE ' + i[1] + ' = ' + '"' + str(
-                                    getattr(obj, col)) + '"' + ';'
-                                data = self.session.execute(sql).cursor.fetchall()
-                                cols = self.session.execute(sql).__dict__['_metadata'].keys
-                                if len(data) == 1:
-                                    dct[self.shorcuts.get(i[0], i[0])] = dict(zip(cols, data[0]))
-                                else:
-                                    dct[self.shorcuts.get(i[0], i[0])] = [dict(zip(cols, i)) for i in data]
-
-                    dct[col] = getattr(obj, col)
-                if self.rels.get(obj.__tablename__):
-                    for i in self.rels[obj.__tablename__]:
-                        obj_arg = list(obj.__dict__['_sa_instance_state'].__dict__ \
-                                           ['manager'].__dict__['local_attrs'][i].__dict__ \
-                                           ['comparator'].__dict__['_parententity'].__dict__ \
-                                           ['_init_properties'][i].__dict__ \
-                                           ['synchronize_pairs'][0][1].__dict__ \
-                                           ['foreign_keys'])[0].__dict__['_column_tokens'][2]
-
-                        rel_obj_arg = list(obj.__dict__['_sa_instance_state'].__dict__['manager'].__dict__ \
-                                               ['local_attrs'][i].__dict__['comparator'].__dict__ \
-                                               ['_parententity'].__dict__['_init_properties'][i].__dict__ \
-                                               ['synchronize_pairs'][0][1].__dict__['foreign_keys'])[0].__dict__ \
-                                          ['parent'].__dict__['_key_label'][len(i) + 1:]
-                        sql = 'SELECT * FROM ' + i + ' WHERE ' + rel_obj_arg + ' = ' + '"' + str(
-                            getattr(obj, obj_arg)) + '"' + ';'
-                        data = self.session.execute(sql).fetchall()
-                        cols = self.session.execute(sql).__dict__['_metadata'].keys
-                        if len(data) == 1:
-                            dct[self.shorcuts.get(i, i)] = self.dump(self.objects[i].query.get(data[0]),
-                                                                     [obj.__tablename__])
-                        else:
-                            dct[self.shorcuts.get(i, i)] = [
-                                self.dump(self.objects[i].query.get(j[0]), [obj.__tablename__]) for j in data]
-                return dct
+        return dct
     
     def dump_route(self, method):
         """
         Special decorator for dumping returned value of your Flask view-function
-        Today - ONLY FOR FLASK!
+        ONLY FOR FLASK!!!
         easy example:
         @app.get('/users/<int:user_id>')
         @kef.dump_route
         def f(user_id):
             return User.query.get(user_id)
         WARNING:
-        `dump_route` must be entre the `route` decorator and view function.
+        `dump_route` must be enter the `route` decorator and view function.
         that has similar sense with this:
         -> https://flask-caching.readthedocs.io/en/latest/#caching-view-functions
         """
         @wraps(method)
         def dump_response(*args, **kwargs):
             content = self.dump(method(*args, **kwargs))
-            if Response is not None:
-                response = Response(dumps(content), mimetype='application/json')
-            else:
+            if Response is None:
                 raise PleaseInstallException('If you want to use `dump_route`, please install Flask!')
+            response = Response(dumps(content), mimetype='application/json')
             return response
         return dump_response
 
