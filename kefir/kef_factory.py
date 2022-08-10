0aa2b2006688012c9acb2ca51b67d5750bb29498
Yourun-Proger <shkrobov.yura@mail.ru>
2022-08-10 21:11:42 +0300

    Fix error msg

--- kefir/kef_factory.py
+++ kefir/kef_factory.py
@@ -5,7 +5,7 @@ from kefir.plugins.base import BasePlugin
 
 class KefirFactory:
     @staticmethod
-    def build_kef(represents=None, plugin='flask', datetime_format='%d.%m.%Y'):
+    def build_kef(represents=None, plugin="flask", datetime_format="%d.%m.%Y"):
         if represents is None:
             represents = {}
 
@@ -14,13 +14,16 @@ class KefirFactory:
                 plugin = PLUGINS[plugin]
             except KeyError:
                 raise ValueError(
-                        f"Plugin name must be one of: {*plugins}"
+                    f"Plugin name must be one of: {PLUGINS}"
                 ) from None
         elif not isinstance(plugin, BasePlugin):
-            raise ValueError(f"Plugin argumnet must be string ({*plugins}) or class")
+            raise ValueError(
+                f"Plugin argumnet must be string from {PLUGINS} or a"
+                "subclass of the BasePlugin class"
+            )
 
         return BaseKefir(
             represents=represents,
             plugin=plugin,
-            datetime_format=datetime_format
+            datetime_format=datetime_format,
         )
